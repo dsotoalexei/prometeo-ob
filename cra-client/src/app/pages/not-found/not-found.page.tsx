c30232a5e34a4b88d895c1e46f2218bb67b795d2
Alexei Diaz <dsoto.alexei@gmail.com>
2022-08-09 09:41:29 -0400

    Fix 404 design

--- cra-client/src/app/pages/not-found/not-found.page.tsx
+++ cra-client/src/app/pages/not-found/not-found.page.tsx
@@ -2,12 +2,37 @@ import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
   return (
-    <div>
-      <h1 style={{ color: 'red', fontSize: 100 }}>404</h1>
-      <h3>Page Not Found</h3>
-      <p>
-        <Link to="/">Go Home</Link>
-      </p>
+    <div
+      className="
+    flex
+    items-center
+    justify-center
+    h-screen
+    bg-gradient-to-r
+    from-indigo-600
+    to-blue-400
+  "
+    >
+      <div className="px-40 py-20 bg-white rounded-md shadow-xl">
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-blue-600 text-9xl">404</h1>
+
+          <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+            <span className="text-red-500">Oops!</span> Página no encontrada
+          </h6>
+
+          <p className="mb-8 text-center text-gray-500 md:text-lg">
+            La página que buscas no existe.
+          </p>
+
+          <Link
+            to="/"
+            className="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100"
+          >
+            Ir al Tablero
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- cra-client/src/libs/ui/components/atoms/navbar/navbar.tsx
+++ cra-client/src/libs/ui/components/atoms/navbar/navbar.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Transition } from '@headlessui/react';
-import { fetchLogout, useAppDispatch } from '../../../../redux';
+import {
+  fetchLogout,
+  isAuthenticatedSelector,
+  useAppDispatch,
+  useAppSelector,
+} from '../../../../redux';
 
 function NavBar() {
   const dispatch = useAppDispatch();
+  const isAuthenticated = useAppSelector(isAuthenticatedSelector);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const onLogOut = () => {
@@ -27,94 +33,98 @@ function NavBar() {
                   />
                 </Link>
               </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                      isActive
-                        ? 'hover:bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium'
-                        : 'text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-                    }
-                  >
-                    Tablero
-                  </NavLink>
+              {isAuthenticated && (
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    <NavLink
+                      to="/"
+                      className={({ isActive }) =>
+                        isActive
+                          ? 'hover:bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+                          : 'text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                      }
+                    >
+                      Tablero
+                    </NavLink>
 
-                  <NavLink
-                    to="/accounts"
-                    className={({ isActive }) =>
-                      isActive
-                        ? 'hover:bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium'
-                        : 'text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-                    }
-                  >
-                    Cuentas
-                  </NavLink>
+                    <NavLink
+                      to="/accounts"
+                      className={({ isActive }) =>
+                        isActive
+                          ? 'hover:bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+                          : 'text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                      }
+                    >
+                      Cuentas
+                    </NavLink>
 
-                  <NavLink
-                    to="/credit-cards"
-                    className={({ isActive }) =>
-                      isActive
-                        ? 'hover:bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium'
-                        : 'text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-                    }
-                  >
-                    Tarjetas de crédito
-                  </NavLink>
-                  <button
-                    type="button"
-                    onClick={onLogOut}
-                    className="text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Cerrar sesion
-                  </button>
+                    <NavLink
+                      to="/credit-cards"
+                      className={({ isActive }) =>
+                        isActive
+                          ? 'hover:bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+                          : 'text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                      }
+                    >
+                      Tarjetas de crédito
+                    </NavLink>
+                    <button
+                      type="button"
+                      onClick={onLogOut}
+                      className="text-gray-300 hover:bg-red-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    >
+                      Cerrar sesion
+                    </button>
+                  </div>
                 </div>
+              )}
+            </div>
+            {isAuthenticated && (
+              <div className="-mr-2 flex md:hidden">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {!isOpen ? (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
               </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
+            )}
           </div>
         </div>
 
